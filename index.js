31430c3dc9744026e5afe93665ca54c7451b900a
Tim <timtran007@gmail.com>
2022-09-29 03:22:39 -0500

    commit tree 8115aae905c852017809bca97eb935c0d03769dc

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+let newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Tim is the champion"
+
+let main = document.querySelector("main#main");
+
+main.remove()
